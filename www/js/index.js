2bf3e52f3597f1fb0af46e8957517c3eb7fda17c
BARJ215 <barj2_15@uni.worc.ac.uk>
2017-11-13 15:12:29 +0000

    Magic8Ball

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,39 @@
 
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
 
-var m;
+
+var q;
+var a = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"  
+];
 
 $(document).on("pagecreate","#pageone",function(){
-  $('#rnButton').on("click", function(){
-      m = random();
-      $('#rn').text(m);
-      console.log(m);
-      if(m==true){
-        navigator.notification.beep(1);
-        navigator.vibrate(200);
-      }else{
-        navigator.notification.beep(2);
-        navigator.vibrate([200,200,200]);
-      }
+  $('#submitButton').on("click", function(){
+      var q = $('#questionText').val();
+      $('#questionAsked').text(q);
+      navigator.notification.alert(a[random()]);
   });            
 });            
 
 function random(){
-	return !Math.round(Math.random());
+	return Math.floor(Math.random()*20);
 }
