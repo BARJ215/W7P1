076425a1ff4cac9ae54c417d22ce51befc2d0cfd
BARJ215 <barj2_15@uni.worc.ac.uk>
2017-11-13 14:18:36 +0000

    beep

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,6 @@
+
 <!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
 
 var m;
 
@@ -6,7 +8,15 @@ $(document).on("pagecreate","#pageone",function(){
   $('#rnButton').on("click", function(){
       m = random();
       $('#rn').text(m);
+      
+      if(m==true){
+        navigator.notification.beep(1); 
+      }else{
+        navigator.notification.beep(2);  
+      }
+      
       console.log(m);
+      
   });            
 });            
 
